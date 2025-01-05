32145201250ab3219ab78c0b1d4a5b0afde7ef15
Mattia Callegari <106533898+callegarimattia@users.noreply.github.com>
2025-01-05 23:30:49 +0100

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"html/template"
+	"os"
+	"time"
+)
+
+func main() {
+	t := template.Must(template.ParseFiles("templates/index.html"))
+	data := struct{ MyAge int }{
+		MyAge: calculateMyAge(),
+	}
+
+	if _, err := os.Stat("/static/index.html"); os.IsNotExist(err) {
+		check(os.MkdirAll("static/", 0700))
+	}
+
+	fp, err := os.Create("static/index.html")
+	check(err)
+	defer fp.Close()
+
+	check(t.Execute(fp, data))
+	check(fp.Sync())
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func calculateMyAge() int {
+	now := time.Now()
+	myBirthDate, _ := time.Parse(time.DateOnly, "2001-04-21")
+	years := now.Year() - myBirthDate.Year()
+
+	if now.YearDay() < myBirthDate.YearDay() {
+		years--
+	}
+
+	return years
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/callegarimattia/callegarimattia.github.io
+
+go 1.23.4
